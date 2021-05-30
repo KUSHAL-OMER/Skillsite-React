07c54d0aea12b60a8a7e14e56431c3eff6db8734
Kushal Omer <kushalomer06@gmail.com>
2021-05-30 09:55:45 +0530

    Final Touch-up

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,40 +2,37 @@ import React, { Component } from 'react';
 import { Card, CardBody, CardHeader, CardText, CardTitle } from 'reactstrap';
 
 class Home extends Component {
-    renderCard ( {con} ) {
-    return(<div className="col-12 col-md-4">
+    renderCard () {
+    return(<div>
         <Card style={{marginBottom: '3%', boxShadow: '5px 5px 5px black'}}>
             <CardHeader style={{textAlign: 'center'}}>
-                <i className="fa fa-user fa-lg"></i>
                 <CardTitle style={{lineHeight: '10px'}}>
-                <p></p>
-                    <p>Name: User</p>
+                <h4>Thought</h4>
                 </CardTitle>
             </CardHeader>
             <CardBody>
-                <CardText>
-                    {con}
+                <CardText style={{textAlign: 'center'}}>
+                    <p>Thinking is the capital, Enterprise is the way, Hard work is the solution</p>
                 </CardText>
             </CardBody>
         </Card>
     </div>);
     }
     render() {
-        const cardcontent = 'Here is the content provided by the users regarding the views for the page.';
-        const card = <this.renderCard con={cardcontent} />
+        const card = <this.renderCard />
         return (
-            <renderCard>
                 <div>
-                    <p className="content">This is the main content of this web page. Here we will display all the main matter of thr home page and content as the views of our users.</p>
-                    <div className="container">
+                    <p className="content">The hard work and an imagination can create anything in this universe. Yes! we human beings can change whole universe, as there is no end of the imagination and also the new growing technologies are increasing our eagerness to know more. It might seems silly but the increasing mentality of human in the field of Science is causing a great impact on his own day to day life.</p>
                         <div className="row">
-                            {card}
-                            {card}
-                            {card}
+                            <div className="col-12 col-md-9">
+                                <p className="content">The increasing demand of new technologies has given a big rise in the field of employment and every person knows that today's world is all about money. All the important stuff that we required is bought by the money that we earn by our hard work. Thus it has become very difficult to grab a job and to maintain it. A person who has nice working experience can handel it easily, but to find such person is a very difficult task for an organization.</p>
+                            </div>
+                            <div className="col">
+                                {card}
+                            </div>
                         </div>
+                        <p className="content">To sort the problem for displaying the work experience of a user to other organizations and to make it easy for organizations to find a well experienced person, it was important to create a platform where the people who are in search of jobs can show their skills. Though there are large number of unemployed people, the organizations are unable to find people with any. Because it is not important that the person is unemployed or free but skillful. The increasing population is making this task more complex, so we have created this website where people can get employment and organizations can able to find employees.</p>
                     </div>
-                </div>
-            </renderCard>
         );
     }
 }
--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -33,13 +33,13 @@ const RenderCard = (props) => {
     });
 
     return ( 
-        <Card style={{marginBottom: '5%'}}>
+        <Card style={{marginBottom: '5%', boxShadow: '5px 5px 20px black'}}>
             <CardHeader style={{textAlign: 'center'}}>
-                <CardImg src={'http://localhost:9000/images/' + props.item.photo} alt={props.item.name} />
+                <CardImg src={'http://localhost:9000/images/' + props.item.photo} alt={props.item.name} style={{borderRadius: '50%'}} />
                 {props.item.name}<br />
                 {props.item.email}
             </CardHeader>
-            <CardBody>
+            <CardBody style={{overflow: 'scroll', height: '180px'}}>
                 {props.item.exp}
             </CardBody>
             <CardFooter>
@@ -60,8 +60,13 @@ const Profile = (props) => {
             </div>);
         });
         return(
-            <div className="container row">
-                {cards}
+            <div>
+                <p className="content">It is very difficult to search a perfect person for the job. In this process an organization always looks for a person who has good theoretical knowledge about the work and also have good hands practically on the related projects. The organization also need to keep in mind that the respect of the employee is far most important, nice name and fame may attracts a large crowd but a skillful and hard working employee always looks for the organization that can provide full respect.</p>
+                <p className="content">While applying to an organization, there are certain things that should be present in the mind of the user. He must maintain the respect of the organization and also of the person who is taking the interview. It is not easily to get selected by an organization for any job, there should be a nice theoretical and practical knowledge in the mind of the employer. Due to the increasing population of the world, the task to get hired by a company is very difficult job. So, always be sure about your ability.</p><br />
+                <h4 style={{textAlign: 'center', textDecoration: 'underline'}}>Here are some Cards about Skills and Work Experience of People Who are in Search of Job</h4><br />
+                <div className="container row">
+                    {cards}
+                </div>
             </div>
         );
     }
